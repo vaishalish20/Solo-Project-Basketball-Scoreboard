ef4e4021a7b4bec0de85d0ddc2dcf36eb33245fa
vsharma <vaishalisharmaukl@gmail.com>
2025-11-14 18:26:24 +0530

    adding functions

---
+++ index.js
@@ -0,0 +1,45 @@
+let leftScoreEl = document.getElementById("left-score");
+let rightScoreEl = document.getElementById("right-score");
+
+let leftScore = 0;
+let rightScore = 0;
+
+function add1ToLeft() {
+    leftScore += 1
+    leftScoreEl.textContent = leftScore;
+
+}
+function add2ToLeft() {
+    leftScore += 2
+    leftScoreEl.textContent = leftScore;
+
+}
+function add4ToLeft() {
+    leftScore += 4
+    leftScoreEl.textContent = leftScore;
+
+}
+function add1ToRight() {
+    rightScore += 1
+    rightScoreEl.textContent = rightScore;
+
+}
+function add2ToRight() {
+    rightScore += 2
+    rightScoreEl.textContent = rightScore;
+
+}
+function add4ToRight() {
+    rightScore += 4
+    rightScoreEl.textContent = rightScore;
+
+}
+function newGame() {
+    leftScoreEl.textContent = 0;
+    rightScoreEl.textContent = 0;
+
+    leftScore = 0;
+    rightScore = 0;
+
+
+}
