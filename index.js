976cf4ec9542495fdd2c2683d48ec9f091cfbe44
vsharma <vaishalisharmaukl@gmail.com>
2025-11-15 19:49:55 +0530

    changing winner color on check function

--- index.js
+++ index.js
@@ -4,41 +4,60 @@ let rightScoreEl = document.getElementById("right-score");
 let leftScore = 0;
 let rightScore = 0;
 
+    const leftHead = document.getElementById("left-head")
+    const rightHead = document.getElementById("right-head")
+
 function add1ToLeft() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     leftScore += 1
     leftScoreEl.textContent = leftScore;
 
 }
 function add2ToLeft() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     leftScore += 2
     leftScoreEl.textContent = leftScore;
 
 }
 function add4ToLeft() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     leftScore += 4
     leftScoreEl.textContent = leftScore;
 
 }
 function minus3ToLeft() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     leftScore -= 3;
     leftScoreEl.textContent = leftScore;
 }
 function add1ToRight() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     rightScore += 1
     rightScoreEl.textContent = rightScore;
 
 }
 function add2ToRight() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     rightScore += 2
     rightScoreEl.textContent = rightScore;
 
 }
 function add4ToRight() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     rightScore += 4
     rightScoreEl.textContent = rightScore;
 
 }
 function minus3ToRight() {
+        leftHead.style.color = "white";
+    rightHead.style.color = "white";
     rightScore -= 3;
     rightScoreEl.textContent = rightScore;
 }
@@ -49,23 +68,26 @@ function newGame() {
     leftScore = 0;
     rightScore = 0;
 
-}
-let leftHead = document.getElementById("left-head")
-let rightHead = document.getElementById("right-head")
+    leftHead.style.color = "white";
+    rightHead.style.color = "white";
 
-console.log(leftHead);
-console.log(rightHead);
+}
+
 
 function Check() {
+
     if (leftScore > rightScore) {
-        console.log("player 1 wins");
-        
+        leftHead.style.color = "green"; // Change to your desired color
+
     }
-    
-    else if (rightScore > leftScore){
-        console.log("player 2 wins");
+
+    else if (rightScore > leftScore) {
+        rightHead.style.color = "green"; // Change to your desired color
     }
-        
-    else
-        alert("Tie")
+
+    else{
+     leftHead.style.color = "yellow";
+    rightHead.style.color = "yellow";
+    }
+   
 }
